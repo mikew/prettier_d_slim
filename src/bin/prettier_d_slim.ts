c5f1e8f9019dea2d8afd75ec9edaf0b0bcb9c53a
Mike Wyatt <wyatt.mike@gmail.com>
2019-12-18 02:23:39 -0400

    commit to prettier_d_slim

--- src/bin/prettier_d_slim.ts
+++ src/bin/prettier_d_slim.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-process.env.CORE_D_TITLE = 'prettier_d'
-process.env.CORE_D_DOTFILE = '.prettier_d'
+process.env.CORE_D_TITLE = 'prettier_d_slim'
+process.env.CORE_D_DOTFILE = '.prettier_d_slim'
 process.env.CORE_D_SERVICE = require.resolve('../linter')
 
 // Needs to be imported after env vars are set.
@@ -12,7 +12,7 @@ function main() {
 
   if (cmd === '-v' || cmd === '--version') {
     console.log(
-      'v%s (prettier_d v%s)',
+      'v%s (prettier_d_slim v%s)',
       require('prettier/package.json').version,
       require('../package.json').version,
     )
