72e10da9cb44c1840b91498e0494075c127d2aed
Mike Wyatt <wyatt.mike@gmail.com>
2019-12-18 02:24:51 -0400

    cleanup nanolru types

--- src/types/nanolru.d.ts
+++ src/types/nanolru.d.ts
@@ -10,18 +10,35 @@ declare module 'nanolru' {
 
   class LRU<T = unknown> {
     constructor(opts?: number | { max?: number; maxAge?: number })
+
+    /** The number of keys currently in the cache. */
     length: number
+
     max: number
     maxAge: number
-    keys: string[]
-    clear: () => void
-    remove: (key: Key) => T
-    peek: (key: Key) => T | undefined
-    set: (key: Key, value: T) => T
-    get: (key: Key) => T | undefined
     cache: Record<Key, CacheEntry<T>>
     head: Key | null
     tail: Key | null
+
+    /** Array of all the keys currently in the cache. */
+    keys: string[]
+
+    /** Clear the cache. */
+    clear: () => void
+
+    /** Remove the value from the cache. */
+    remove: (key: Key) => T | undefined
+
+    /** Query the value of the key without marking the key as most recently used. */
+    peek: (key: Key) => T | undefined
+
+    /** Set the value of the key and mark the key as most recently used. */
+    set: (key: Key, value: T) => T
+
+    /** Query the value of the key and mark the key as most recently used. */
+    get: (key: Key) => T | undefined
+
+    evict: () => void
   }
 
   export default LRU
